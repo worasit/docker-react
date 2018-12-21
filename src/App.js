2ffae94b47197dce0859b8a6d7b2587d979e2c98
worasit daimongkol <worasit.dmk501@gmail.com>
2018-12-21 11:15:06 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hi There
           </a>
         </header>
       </div>
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing 1', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
